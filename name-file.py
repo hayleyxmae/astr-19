51390ffe679d19e846983b7c6dc5b3aba4817a33
Hayley-Mae Harlan <72539584+hayleyxmae@users.noreply.github.com>
2024-01-11 12:35:13 -0800

    Add files via upload
    this is part of code journal 1, my introduction

---
+++ name-file.py
@@ -0,0 +1,13 @@
+#this program is write my introduction
+
+#this function tells the computer to print my introduction
+def Printintro():
+	print("Mae Harlan she/they	My favorite movie is The Rescuers. My favorite food is char siu bao.")
+
+#this defines the main() function for the program
+def main():
+	Printintro()
+
+#this executes first when you run the program
+if __name__=="__main__":
+	main()
