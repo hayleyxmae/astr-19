57272d888ec371f1921c387e17cd0457758119c3
Hayley-Mae Harlan <72539584+hayleyxmae@users.noreply.github.com>
2024-01-25 16:18:23 -0800

    Add files via upload
    code journal 2

---
+++ code-journal-2.py
@@ -0,0 +1,23 @@
+#this will add two floats
+x = 2.0
+y = 3.0
+print("x+y = ", x+y)
+
+#this will report the data type for x+y
+print("type(x+y): ",type(x+y))
+
+#this will subtract two integers
+a = 3
+b = 2
+print("a-b = ",a-b)
+
+#this will report the data type for a-b
+print("type(a-b): ",type(a-b))
+
+#this will multiply an integer and a float
+c = 5
+d = 4.0
+print("c*d = ",c*d)
+
+#this will report the data type for the product
+print("type(c*d): ",type(c*d))
